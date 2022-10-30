789193c60d4da1c6ab938dda508e9320cb1c60d7
Jana, Subhadeep <subjana@iu.edu>
2022-10-30 14:39:35 -0400

    Delete index.js

--- PageTurnerSanity/pageturner_frontend/components/index.js
+++
@@ -1,4 +0,0 @@
-export { default as Sidebar } from './Sidebar';
-export { default as UserProfile } from './UserProfile';
-export { default as Login } from './Login';
-export { default as ForgotPassword } from './ForgotPassword';
