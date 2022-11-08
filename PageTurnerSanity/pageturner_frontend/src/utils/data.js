fddbd5a086dae147a4bd0ee7d38b8e8e398e020a
Jana, Subhadeep <subjana@iu.edu>
2022-11-08 13:16:23 -0500

    Add files via upload

--- PageTurnerSanity/pageturner_frontend/src/utils/data.js
+++ PageTurnerSanity/pageturner_frontend/src/utils/data.js
@@ -1,56 +1,221 @@
 export const categories = [
     {
       name: 'Fantasy',
-      image: 'https://i.pinimg.com/564x/05/61/aa/0561aa0b2a39d0a93b9386bcf6a5235d.jpg',
+      image: 'https://t3.ftcdn.net/jpg/02/81/42/82/360_F_281428216_YWRTOqeBWBmtuWxBci02ClnEnI22Fh7e.jpg',
     },
     {
       name: 'Science Fiction',
-      image: 'https://i.pinimg.com/236x/25/14/29/251429345940a47490cc3d47dfe0a8eb.jpg',
+      image: 'https://assets.telegraphindia.com/telegraph/0b14c9c9-63b9-482a-a66d-c81be41ae79f.jpg',
     },
     {
       name: 'Dystopian',
-      image: 'https://i.pinimg.com/236x/03/48/b6/0348b65919fcbe1e4f559dc4feb0ee13.jpg',
+      image: 'https://edsurge.imgix.net/uploads/post/image/14892/Shutterstock_Superstar-1657816027.jpg?auto=compress%2Cformat&w=1024&h=512&fit=crop',
     },
     {
       name: 'Action & Adventure',
-      image: 'https://i.pinimg.com/750x/66/b1/29/66b1296d36598122e6a4c5452b5a7149.jpg',
+      image: 'https://wwwnc.cdc.gov/travel/images/mountain-biker.jpg',
     },
     {
       name: 'Mystery',
-      image: 'https://i.pinimg.com/236x/72/8c/b4/728cb43f48ca762a75da645c121e5c57.jpg',
+      image: 'https://celadonbooks.com/wp-content/uploads/2020/03/what-is-a-mystery.jpg',
     },
     {
       name: 'Horror',
-      image: 'https://i.pinimg.com/236x/7d/ef/15/7def15ac734837346dac01fad598fc87.jpg',
+      image: 'https://anotherimg-dazedgroup.netdna-ssl.com/779/azure/another-prod/410/6/416736.jpg',
     },
     {
       name: 'Thriller & Suspense',
-      image: 'https://i.pinimg.com/236x/b9/82/d4/b982d49a1edd984c4faef745fd1f8479.jpg',
+      image: 'https://images-cdn.reedsy.com/discovery/post/48/featured_image/medium_27767f2a8c2f2a2cc5a8b04a345b67a007335e61.jpg',
     },
     {
       name: 'History',
-      image: 'https://i.pinimg.com/736x/f4/e5/ba/f4e5ba22311039662dd253be33bf5f0e.jpg',
-    }, {
+      image: 'https://alameda.edu/wp-content/uploads/2021/07/History.png',
+    }, 
+    {
       name: 'Romance',
-      image: 'https://i.pinimg.com/236x/fa/95/98/fa95986f2c408098531ca7cc78aee3a4.jpg',
+      image: 'https://cdn.pixabay.com/photo/2020/03/21/02/46/romantic-4952533_1280.jpg',
     },
     {
       name: 'Poems',
       image: 'https://i.pinimg.com/236x/46/7c/17/467c17277badb00b638f8ec4da89a358.jpg',
-    }, {
+    }, 
+    {
       name: 'Short Stories',
-      image: 'https://i.pinimg.com/236x/6c/3c/52/6c3c529e8dadc7cffc4fddedd4caabe1.jpg',
-    }, {
+      image: 'https://images-production.bookshop.org/spree/lists/hero_images/84919/original/shortstorypanel1.jpg?1620307551',
+    },
+    {
       name: 'Biography',
-      image: 'https://i.pinimg.com/236x/1b/c8/30/1bc83077e363db1a394bf6a64b071e9f.jpg',
+      image: 'https://cdn.profoto.com/cdn/053149e/contentassets/d39349344d004f9b8963df1551f24bf4/profoto-albert-watson-steve-jobs-pinned-image-original.jpg?width=1280&quality=75&format=jpg',
     },
     {
       name: 'Art',
-      image: 'https://i.pinimg.com/236x/2e/63/c8/2e63c82dfd49aca8dccf9de3f57e8588.jpg',
-    },
+      image: 'https://cdn.pixabay.com/photo/2016/12/15/20/21/texture-1909992__480.jpg',
+    }, 
   ];
 
-export const userQuery = (userId) => {
+  export const feedQuery = `*[_type == "pin"] | order(_createdAt desc) {
+    image{
+      asset->{
+        url
+      }
+    },
+        _id,
+        destination,
+        postedBy->{
+          _id,
+          userName,
+          image
+        },
+        save[]{
+          _key,
+          postedBy->{
+            _id,
+            userName,
+            image
+          },
+        },
+      } `;
+  
+  export const pinDetailQuery = (pinId) => {
+    const query = `*[_type == "pin" && _id == '${pinId}']{
+      image{
+        asset->{
+          url
+        }
+      },
+      _id,
+      title, 
+      about,
+      category,
+      destination,
+      postedBy->{
+        _id,
+        userName,
+        image
+      },
+     save[]{
+        postedBy->{
+          _id,
+          userName,
+          image
+        },
+      },
+      comments[]{
+        comment,
+        _key,
+        postedBy->{
+          _id,
+          userName,
+          image
+        },
+      }
+    }`;
+    return query;
+  };
+  
+  export const pinDetailMorePinQuery = (pin) => {
+    const query = `*[_type == "pin" && category == '${pin.category}' && _id != '${pin._id}' ]{
+      image{
+        asset->{
+          url
+        }
+      },
+      _id,
+      destination,
+      postedBy->{
+        _id,
+        userName,
+        image
+      },
+      save[]{
+        _key,
+        postedBy->{
+          _id,
+          userName,
+          image
+        },
+      },
+    }`;
+    return query;
+  };
+  
+  export const searchQuery = (searchTerm) => {
+    const query = `*[_type == "pin" && title match '${searchTerm}*' || category match '${searchTerm}*' || about match '${searchTerm}*']{
+          image{
+            asset->{
+              url
+            }
+          },
+              _id,
+              destination,
+              postedBy->{
+                _id,
+                userName,
+                image
+              },
+              save[]{
+                _key,
+                postedBy->{
+                  _id,
+                  userName,
+                  image
+                },
+              },
+            }`;
+    return query;
+  };
+  
+  export const userQuery = (userId) => {
     const query = `*[_type == "user" && _id == '${userId}']`;
+    return query;
+  };
+  
+  export const userCreatedPinsQuery = (userId) => {
+    const query = `*[ _type == 'pin' && userId == '${userId}'] | order(_createdAt desc){
+      image{
+        asset->{
+          url
+        }
+      },
+      _id,
+      destination,
+      postedBy->{
+        _id,
+        userName,
+        image
+      },
+      save[]{
+        postedBy->{
+          _id,
+          userName,
+          image
+        },
+      },
+    }`;
+    return query;
+  };
+  
+  export const userSavedPinsQuery = (userId) => {
+    const query = `*[_type == 'pin' && '${userId}' in save[].userId ] | order(_createdAt desc) {
+      image{
+        asset->{
+          url
+        }
+      },
+      _id,
+      destination,
+      postedBy->{
+        _id,
+        userName,
+        image
+      },
+      save[]{
+        postedBy->{
+          _id,
+          userName,
+          image
+        },
+      },
+    }`;
     return query;
   };
