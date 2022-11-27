5f523fd4f32ba06de31a83813e40d9cf086da8d0
Patil, Mrunal Yogesh <mrupatil@iu.edu>
2022-11-27 16:16:00 -0500

    Delete authenticate.py

--- authenticate.py
+++
@@ -1,5 +0,0 @@
-email = "mrunal@gmail.com"
-dateOfBirth = "11-11-1998"
-temp = dateOfBirth[len(dateOfBirth)-2]+dateOfBirth[len(dateOfBirth)-1]+dateOfBirth[0]+dateOfBirth[1]
-username = email.split('@')
-print(username[0]+temp)
